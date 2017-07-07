bae4d160bc1cc3949f35ff3a5a4ea17b574609ae
Niki <conteo_1987@abv.bg>
2017-07-07 16:24:03 +0300

    Problem 2. Book Shop

--- Book Shop/GoldenEditionBook.cs
+++ Book Shop/GoldenEditionBook.cs
@@ -1,4 +1,12 @@
-﻿public class GoldenEditionBook
+﻿public class GoldenEditionBook : Book
 {
+    public GoldenEditionBook(string author, string title, decimal price) : base(author, title, price)
+    {
 
+    }
+
+    public override decimal Price
+    {
+        get { return base.Price * 1.3m; }
+    }
 }
