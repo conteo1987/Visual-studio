6d4afd73a27bbefba2a8e238248711fd1f69d03c
Niki <conteo_1987@abv.bg>
2017-07-19 22:08:08 +0300

    Interfaces and Abstraction - Lab

---
+++ Lab Interfaces and Abstraction/Cars/Tesla.cs
@@ -0,0 +1,28 @@
+﻿public class Tesla : IElectricCar, ICar
+{
+    public Tesla(string model, string color, int battery)
+    {
+
+        this.Model = model;
+        this.Color = color;
+        this.Battery = battery;
+    }
+
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public int Battery { get; private set; }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+    }
+}
---
+++ Lab Interfaces and Abstraction/Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Rectangle : IDrawable
+{
+
+
+    private int height;
+    private int width;
+
+    public int Height
+    {
+        get { return height; }
+        protected set { height = value; }
+    }
+
+    public int Width
+    {
+        get { return width; }
+        protected set { width = value; }
+    }
+
+    public Rectangle(int height, int width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+
+
+    public void Draw()
+    {
+        DrawLine(this.Width, '*', '*');
+        for (int i = 1; i < this.Height - 1; ++i)
+            DrawLine(this.Width, '*', ' ');
+        DrawLine(this.Width, '*', '*');
+    }
+    private void DrawLine(int width, char end, char mid)
+    {
+        Console.Write(end);
+        for (int i = 1; i < width - 1; ++i)
+            Console.Write(mid);
+        Console.WriteLine(end);
+
+    }
+}
