40b467bc7c414443434b88cb40322ed738015b79
Niki <conteo_1987@abv.bg>
2017-07-27 18:47:18 +0300

    Lab Iterators and Comparators

---
+++ Lab Iterators and Comparators/Library/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Startup
+ {
+     static void Main(string[] args)
+    {
+        // PROBLEM 4 ------------------------------------------------------------------------------------------
+        //Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        //Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+        //Book bookThree = new Book("The Documents in the Case", 1930);
+        // Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+        //foreach (var book in libraryTwo)
+        //{
+        //    Console.WriteLine(book);
+        //}
+
+        // PROBLEM 2 and 3 -----------------------------------------------------------------------------------------------
+        //Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        //Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+        //Book bookThree = new Book("The Documents in the Case", 1930);
+
+        //Library libraryOne = new Library();
+        //Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+        //foreach (var book in libraryTwo)
+        //{
+        //    Console.WriteLine(book);
+        //}
+
+
+
+        // PROBLEM 1 ---------------------------------------------------------------------------------------
+        //Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        //Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+        //Book bookThree = new Book("The Documents in the Case", 1930);
+
+        //Library libraryOne = new Library();
+        //Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+    }
+}
+
