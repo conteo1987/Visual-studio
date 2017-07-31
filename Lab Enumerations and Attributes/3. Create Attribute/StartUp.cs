99e710b1d704a731a5170ceaaab1649950bcb3bb
Niki <conteo_1987@abv.bg>
2017-07-31 23:03:13 +0300

    Lab: Enumerations and Attributes

---
+++ Lab Enumerations and Attributes/3. Create Attribute/StartUp.cs
@@ -0,0 +1,10 @@
+﻿[SoftUni("Ventsi")]
+public class StartUp
+{[SoftUni("Goshko")]
+    static void Main(string[] args)
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
+
---
+++ Lab Enumerations and Attributes/Enumerations/WeeklyCalendar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using static StartUp;
+
+public class WeeklyCalendar
+{
+    private IList<WeeklyEntry> entries;
+
+    public WeeklyCalendar()
+    {
+        this.entries = new List<WeeklyEntry>();
+    }
+
+    public IEnumerable<WeeklyEntry> WeeklySchedule => this.entries;
+
+    public void AddEntry(string weekday, string notes)
+    {
+        this.entries.Add(new WeeklyEntry(weekday, notes));
+    }
+}
+
